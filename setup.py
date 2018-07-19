287ab422d74c038f420d119f97a044a91897903c
Janahan Selvanayagam <seljanahan@hotmail.com>
2018-07-18 22:21:20 -0400

    add install requirements

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ from setuptools import setup, find_packages, Extension
 setup(
     name="dlab",
     packages=['dlab'],
-    version="0.0.1",
+    version="0.1.4",
     description="Python code for Dwivedi Lab",
     author='Janahan Selvanayagam',
     author_email='seljanahan@hotmail.com',
     url='https://github.com/dwivedilab/dlab',
     keywords=['EEG'],
-    install_requires=[],
+    install_requires=[
+        'pandas',
+        'savReaderWriter',
+        'matplotlib',
+        'numpy',
+        'xlrd'
+    ],
     include_package_data=True,
     zip_safe=False
 )
-
-#entry_points={'console_scripts': ['pyinstrument = pyinstrument.__main__:main']},
