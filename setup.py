f87b8ae63ca23a256ebcc12e6c89c3294b6aad27
Janahan Selvanayagam <seljanahan@hotmail.com>
2018-10-08 22:20:40 -0400

    v1.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages, Extension
 setup(
     name="dlab",
     packages=['dlab'],
-    version="1.0.0",
+    version="1.0.2",
     description="Python code for Dwivedi Lab",
     author='Janahan Selvanayagam',
     author_email='seljanahan@hotmail.com',
